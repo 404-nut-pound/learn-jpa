53aecf6cce30c7ac1e93c003c54a7bfd74dace4e
Skynet <souls4523@gmail.com>
2022-04-17 22:48:05 +0900

    양방향 매핑 설정

--- src/main/java/jpa/team/Team.java
+++ src/main/java/jpa/team/Team.java
@@ -1,9 +1,12 @@
 package jpa.team;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.Data;
 
@@ -16,4 +19,6 @@ public class Team {
   @Column(name = "team_id")
   private Long id;
   private String name;
+  @OneToMany(mappedBy = "team")
+  private List<Member> memberList;
 }
