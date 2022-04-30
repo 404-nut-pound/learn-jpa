71a0552f0ffe39a5ac2d65baa3a174e31434d5a2
khs <souls4523@gmail.com>
2022-05-01 01:19:18 +0900

    회원 등록 및 목록 조회 기능 개발

--- src/main/java/io/hskim/learnjpapart2/domain/Member.java
+++ src/main/java/io/hskim/learnjpapart2/domain/Member.java
@@ -2,25 +2,27 @@ package io.hskim.learnjpapart2.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Data
 @Setter(value = AccessLevel.NONE)
 @Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
-
   @Id
   @GeneratedValue
   @Column(name = "member_id")
@@ -28,6 +30,7 @@ public class Member {
 
   private String name;
 
+  @Embedded
   private Address Address;
 
   @OneToMany(mappedBy = "member", fetch = FetchType.LAZY) // 양방향 매핑의 경우 연관 관계의 주도권이 없는 쪽에 mappedBy 설정
